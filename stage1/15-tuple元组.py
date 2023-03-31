db3486e294435b80754fd4d3e80ddbba793e1db1
terry <guochi.qin@outlook.com>
2023-03-31 17:35:06 +0800

    update

---
+++ stage1/15-tuple元组.py
@@ -0,0 +1,35 @@
+"""
+    什么是 tuple：只读的 list 就是 tuple
+
+    tuple 的定义语法：
+        变量名 = (元素1, 元素2, 元素3, ...)
+
+        可以通过如下当时定义空元组：
+            变量名 = ()
+            变量名 = tuple()
+
+    tuple 的常用方法：
+        元组.index(element)：查找指定元素，如果元素在元组中存在则返回其索引，不存在则报错；
+        元组.count(element)：统计指定元素在当前元组中出现的次数；
+        len(元组)：返回指定元组中元素的个数；
+"""
+
+
+user_info = ("terry", 18, 50.5, ("篮球", "足球", "排球"))
+
+for item in user_info:
+    print(item)
+
+# user_info[0] = "tim"
+
+# 元组.index(element)：查找指定元素，如果元素在元组中存在则返回其索引，不存在则报错
+index = user_info.index(50.5)
+print(f"获取元素索引：{index}")
+
+# 元组.count(element)：统计指定元素在当前元组中出现的次数
+count = user_info.count(("篮球", "足球", "排球"))     # 查找子元素时，子元素必须完全一样才会视为同一个元素！！！
+print(f"统计元素出现的次数：{count}")
+
+# len(元组)：返回指定元组中元素的个数
+count = len(user_info)
+print(f"获取元组中元素的总个数：{count}")
