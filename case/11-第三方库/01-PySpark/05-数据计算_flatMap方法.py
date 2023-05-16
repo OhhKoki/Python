be22f6aa62bc4f7a2fbfec12cce384af06ad2362
terry <guochi.qin@outlook.com>
2023-05-16 13:14:21 +0800

    update

--- case/11-第三方库/01-PySpark/05-数据计算_flatMap方法.py
+++ case/11-第三方库/01-PySpark/05-数据计算_flatMap方法.py
@@ -8,17 +8,12 @@ conf = SparkConf().setMaster("local[*]").setAppName("test_pyspark_app")
 context = SparkContext(conf=conf)
 
 # 4、数据输入：通过 parallelize 将 Python 的数据容器转为 RDD
-container = [1, 2, 3, 4, 5]
+container = ["i am terry", "who am i", "i am terry"]
 rdd = context.parallelize(container)
-
-
-def fun(data):
-    return data * 10
-
-
-# 需求：每个数字都变大 10 倍，然后加 5
-# 可以通过链式调用的方式多次调用 map 方法，返回一个新的 RDD 对象（原始的 RDD 对象内容不变！！！）
-rddMap = rdd.map(fun).map(lambda x: x + 5)
+# 将 RDD 中的每个单词都提取出来
+rddMap = rdd.flatMap(lambda x: x.split(" "))
+rddFlatMap = rdd.flatMap(lambda x: x.split(" "))
 
 # 5、输出 RDD
 print(f"rddMap value is: {rddMap.collect()}")
+print(f"rddFlatMap value is: {rddFlatMap.collect()}")
